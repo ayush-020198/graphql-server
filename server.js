e1293c4571e554ce951fe5cf0ee50a2ec3c549dd
Ayush Shrivastava <ayush.shrivastavaujn@gmail.com>
2020-06-23 12:52:56 +0530

    second commit

--- server.js
+++ server.js
@@ -3,22 +3,58 @@ const expressGraphQL = require('express-graphql');
 const {
     GraphQLSchema, 
     GraphQLObjectType,
-    GraphQLString
+    GraphQLString, 
+    GraphQLList,
+    GraphQLInt,
+    GraphQLNonNull
 } = require('graphql');
 const app = express();
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'HelloWorld',
-        fields: () => ({
-            message: {
-                type: GraphQLString,
-                resolve: () => 'Hello World'
-            }       
-        })
+
+const authors = [
+	{ id: 1, name: 'J. K. Rowling' },
+	{ id: 2, name: 'J. R. R. Tolkien' },
+	{ id: 3, name: 'Brent Weeks' }
+]
+
+const books = [
+	{ id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
+	{ id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
+	{ id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1 },
+	{ id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
+	{ id: 5, name: 'The Two Towers', authorId: 2 },
+	{ id: 6, name: 'The Return of the King', authorId: 2 },
+	{ id: 7, name: 'The Way of Shadows', authorId: 3 },
+	{ id: 8, name: 'Beyond the Shadows', authorId: 3 }
+]
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    description: 'Name of book with its author',
+    fields: () => ({
+        id: {type: GraphQLNonNull(GraphQLInt)},
+        name: {type: GraphQLNonNull(GraphQLString)},
+        authorId: {type: GraphQLNonNull(GraphQLInt)}
     })
 })
+
+const RootQueryType = new GraphQLObjectType({
+name: 'Query',
+description: 'Root Query',
+fields: () => ({
+    books: {
+        type: new GraphQLList(BookType),
+        description: 'List of all the books',
+        resolve: () => books
+    }
+})
+})
+
+const schema = new GraphQLSchema({
+    query: RootQueryType
+})
+
 app.use('/graphql', expressGraphQL({
-    schema: schema,
+    schema:schema,
     graphiql: true
 }))
 app.listen(5000, console.log('Running successfully'));
