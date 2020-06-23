7f0caef9735ea8ceb5a3e6015d0bcd02437bf6a3
Ayush Shrivastava <ayush.shrivastavaujn@gmail.com>
2020-06-23 14:01:00 +0530

    mutation initialize

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ const BookType = new GraphQLObjectType({
         authorId: {type: GraphQLNonNull(GraphQLInt)},
         authors: {
             type: AuthorType,
-            resolve: (book) => authors.find(author => author.id === book.authorId)
+            resolve: (book) => {return authors.find(author => author.id === book.authorId)}
         }
     })
 })
@@ -58,6 +58,14 @@ const RootQueryType = new GraphQLObjectType({
 name: 'Query',
 description: 'Root Query',
 fields: () => ({
+    book: {
+        type: BookType,
+        description: 'A single book related information',
+        args: {
+            id: { type: GraphQLInt }
+        },
+        resolve: (parent, args) => books.find(book => book.id === args.id)
+    },
     books: {
         type: new GraphQLList(BookType),
         description: 'List of all the books',
@@ -67,6 +75,14 @@ fields: () => ({
         type: new GraphQLList(AuthorType),
         description: 'List of all the authors',
         resolve: () => authors
+    },
+    author: {
+        type: AuthorType,
+        description: 'A single author related information',
+        args: {
+            id: { type: GraphQLInt }
+        },
+        resolve: (parent, args) => authors.find(author => author.id === args.id)
     }
 })
 })
